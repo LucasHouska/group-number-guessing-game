c613c662c96a531eff831d230a654cad2370e321
Lucas Houska <ljhouska75@gmail.com>
2022-03-10 13:32:04 -0600

    POST, GET and ajax working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,55 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  $('#submitBtn').on('click', handleSubmit);
+
+  getGuesses();
+}
+
+//get ajax
+function getGuesses(){
+  console.log('getting guesses...');
+  
+  $.ajax({
+    url: '/guess',
+   method: 'GET'
+  }).then(function(response){
+    console.log(response);
+    render(response);
+  }).catch(function(error){
+    console.log(error);
+    alert('error in get!')
+  })
+  console.log('end of get function...');
+}
+
+// POST ajax
+function handleSubmit (){
+  console.log('clicked in handleSubmit');
+  let lucasGuess = $('#lucasGuess').val();
+  let julietteGuess = $('#julietteGuess').val();
+  let anissaGuess = $('#anissaGuess').val();
+  
+  $.ajax({
+    url: '/guess',
+   method: 'POST',
+   data: {
+     lucasGuess: lucasGuess,
+     julietteGuess: julietteGuess,
+     anissaGuess: anissaGuess
+   }
+  }).then(function(response) {
+    console.log('response');
+    getGuesses();
+    $('input').val('');
+  }).catch(function(error){
+    console.log(error);
+  })
+}
+
+function render(guesses){
+  for (const guess of guesses){
+    $('#guess').append(`<p>${guess.lucasGuess} ${guess.julietteGuess} ${guess.anissaGuess}</p>`)
+  }
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const res = require('express/lib/response');
 const app = express();
 const PORT = 5000;
 
@@ -9,9 +10,29 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+const guesses = [];
+
 // GET & POST Routes go here
 
+//get data
+app.get('/guess', (req, res) => {
+  console.log('GET /guesses!');
+  res.send(guesses);
+})
 
+app.post('/guess', (req, res) => {
+  console.log('POST /guesses', req.body);
+
+  guesses.push(req.body);
+
+  res.sendStatus(201);
+  console.log(guesses);
+})
+
+
+//starts the server 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
+
+
